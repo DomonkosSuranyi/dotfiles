308e9af84f23eac4c37e57cfcfe66dff3c1f6567
Domonkos Suranyi <domonkos.suranyi@omixon.com>
2019-09-27 13:56:28 +0200

    Battery notification script

---
+++ bin/battery_notif.py
@@ -0,0 +1,14 @@
+#!/bin/python
+
+import subprocess
+import os
+
+acpi_output = subprocess.Popen(['/usr/bin/acpi', '-b'], stdout=subprocess.PIPE).communicate()
+acpi_output = acpi_output[0].decode('utf-8').split('\n')
+index_of_discharging = acpi_output[0].find('Discharging')
+if index_of_discharging > -1:
+  index_of_percentage = acpi_output[0].find('%')
+  percentage = acpi_output[0][index_of_discharging + 13 : index_of_percentage]
+  if int(percentage) < 10:
+    os.system('DISPLAY=:0.0 /usr/bin/notify-send "Low battery" "The batery is almost depleted" -u critical')
+
